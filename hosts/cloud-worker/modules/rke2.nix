8bc5e4966b61e52936d429fe3c7f717de98519f6
youmin1017 <youmin.main@gmail.com>
2025-05-06 01:34:03 +0800

    fix: wrong server addr

--- hosts/cloud-worker/modules/rke2.nix
+++ hosts/cloud-worker/modules/rke2.nix
@@ -4,7 +4,7 @@
     enable = true;
     role = "agent";
 
-    serverAddr = "10.22.23.215";
+    serverAddr = "https://10.22.23.215:9345";
     tokenFile = config.sops.secrets."rke2/node_token".path;
   };
 
