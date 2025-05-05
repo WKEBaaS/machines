09a419cd619b800d8b63bdc089595edb1779e34d
youmin1017 <youmin1017@wke.csie.ncnu.edu.tw>
2025-05-05 15:36:18 +0800

    fix: boot

--- hosts/cloud-worker/configuration.nix
+++ hosts/cloud-worker/configuration.nix
@@ -26,8 +26,9 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
   nix.settings.auto-optimise-store = true;
   nix.settings.experimental-features = [
