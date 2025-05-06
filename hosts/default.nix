b4d440e7966b1e2371c44a1e75a796a3a0e86116
youmin1017 <youmin.main@gmail.com>
2025-05-06 11:04:32 +0800

    add: worker 03

--- hosts/default.nix
+++ hosts/default.nix
@@ -53,6 +53,10 @@ in
           name = "cloud-worker-02";
           config = ./cloud-worker/configuration.nix;
         }
+        {
+          name = "cloud-worker-03";
+          config = ./cloud-worker/configuration.nix;
+        }
       ];
     in
     builtins.listToAttrs (
