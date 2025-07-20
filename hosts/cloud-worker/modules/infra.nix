dbce86500e2137d97ea99232f9cbf8c407f33213
youmin1017 <youmin1017@wke.csie.ncnu.edu.tw>
2025-07-20 12:16:47 +0800

    fix: server ip changed

--- hosts/cloud-worker/modules/infra.nix
+++ hosts/cloud-worker/modules/infra.nix
@@ -25,7 +25,7 @@
     enable = true;
     role = "agent";
 
-    serverAddr = "https://10.22.23.215:9345";
+    serverAddr = "https://10.22.21.131:9345";
     tokenFile = config.sops.secrets."rke2/node_token".path;
   };
 
