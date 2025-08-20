44f6da8f738e4e753a430c7a0c4cf59c83cd3101
youmin1017 <youmin.main@gmail.com>
2025-08-20 16:32:09 +0800

    feat: new node

--- hosts/cloud-worker/modules/infra.nix
+++ hosts/cloud-worker/modules/infra.nix
@@ -25,7 +25,7 @@
     enable = true;
     role = "agent";
 
-    serverAddr = "https://10.22.23.215:9345";
+    serverAddr = "https://10.22.24.176:9345";
     tokenFile = config.sops.secrets."rke2/node_token".path;
   };
 
