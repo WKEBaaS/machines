daf7e87ba83fdaa9aebd9243f7afa1dde6698d0c
youmin1017 <youmin.main@gmail.com>
2025-08-20 15:47:08 +0800

    reafact: remove unused

--- hosts/cloud-server/modules/system.nix
+++ hosts/cloud-server/modules/system.nix
@@ -1,33 +1,5 @@
-{
-  config,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
-
-  fileSystems =
-    let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      secretPath = config.sops.secrets."wke/syncwke_secret".path;
-
-      mountPoints = [
-        "home"
-        "Public"
-        "EAS_RW"
-      ];
-    in
-    builtins.listToAttrs (
-      map (mountPoint: {
-        name = "/mnt/syncwke/${mountPoint}";
-        value = {
-          device = "//syncwke.csie.ncnu.edu.tw/${mountPoint}";
-          fsType = "cifs";
-          options = [ "${automount_opts},credentials=${secretPath}" ];
-        };
-      }) mountPoints
-    );
-
-  sops.secrets."wke/syncwke_secret" = { };
 }
